d8d418bc88f3e0e6fe1a6ce8d593afc39adc056a
Caio1213 <caiofxpgm@gmail.com>
2025-02-05 16:58:15 -0300

    edit dados do cardData

--- src/components/CardInicio.tsx
+++ src/components/CardInicio.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import professor from "../assets/professor.jpg";
@@ -11,42 +12,49 @@ type conteudoCard = {
   image: object;
   id: number;
   paragraph: string;
+  paragraph2: string;
 };
+
+export const cardData: conteudoCard[] = [
+  {
+    title: "Aulas para Todos os Níveis",
+    image: {},
+    id: 1,
+    paragraph: "Desde iniciantes até músicos avançados.",
+    paragraph2: " Seja você um iniciante que nunca pegou um instrumento ou um músico avançado em busca de aperfeiçoamento, temos o curso ideal para o seu nível. "
+  },
+  {
+    title: "Professores Experientes",
+    image: { professor },
+    id: 2,
+    paragraph: "Músicos profissionais com anos de experiência no ensino.",
+    paragraph2: " Cada professor traz não apenas conhecimento técnico, mas também paixão pela música, garantindo aulas envolventes, dinâmicas e inspiradoras."
+  },
+  {
+    title: "Variedade de Instrumentos",
+    image: {},
+    id: 3,
+    paragraph: "Violão, piano, violino, bateria, canto e mais!",
+    paragraph2: " Se você ainda não sabe qual instrumento escolher, nossos professores podem ajudá-lo a encontrar o que mais combina com seu estilo e personalidade musical."
+  },
+  {
+    title: "Adaptável a sua rotina",
+    image: {},
+    id: 4,
+    paragraph: "Aulas presenciais e online,  adaptadas à sua rotina.",
+    paragraph2: " Sabemos que a rotina pode ser corrida, por isso oferecemos horários flexíveis para que você possa aprender no seu próprio tempo. "
+  },
+  {
+    title: "Ambiente Aconchegante",
+    image: {},
+    id: 5,
+    paragraph: "Espaços modernos e totalmente equipados.",
+    paragraph2: " Contamos com salas equipadas com instrumentos de alta qualidade, tratamento acústico e um ambiente acolhedor que estimula a criatividade."
+  },
+];
 
 function CardInicio() {
   const [slideView, setSlidesView] = useState(3)
-  const cardContent: conteudoCard[] = [
-    {
-      title: "Aulas para Todos os Níveis",
-      image: {},
-      id: 1,
-      paragraph: "Desde iniciantes até músicos avançados.",
-    },
-    {
-      title: "Professores Experientes",
-      image: { professor },
-      id: 2,
-      paragraph: "Músicos profissionais com anos de experiência no ensino.",
-    },
-    {
-      title: "Variedade de Instrumentos",
-      image: {},
-      id: 3,
-      paragraph: "Violão, piano, violino, bateria, canto e mais!",
-    },
-    {
-      title: "Adaptável a sua rotina",
-      image: {},
-      id: 4,
-      paragraph: "Aulas presenciais e online,  adaptadas à sua rotina.",
-    },
-    {
-      title: "Ambiente Aconchegante",
-      image: {},
-      id: 5,
-      paragraph: "Espaços modernos e totalmente equipados.",
-    },
-  ];
 
   const navigate = useNavigate();
 
@@ -75,7 +83,7 @@ function CardInicio() {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        {cardContent.map((card) => (
+        {cardData.map((card) => (
           <SwiperSlide key={card.id} className="cardContent">
             <div className="cardContentData">
               <h4>{card.title}</h4>
